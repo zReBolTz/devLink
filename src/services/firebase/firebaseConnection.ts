d6dd1d3acf651b45206d7ae765d95a2fec0feced
zReBolTz <ferreiraw36@gmail.com>
2025-02-05 22:00:23 -0300

    feat: Adicionando variaveis de ambiente

--- src/services/firebase/firebaseConnection.ts
+++ src/services/firebase/firebaseConnection.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCtfgo8yOlLB-ZZMf_Zh6py1e1-AhKroUE",
-  authDomain: "devlink-d8844.firebaseapp.com",
-  projectId: "devlink-d8844",
-  storageBucket: "devlink-d8844.firebasestorage.app",
-  messagingSenderId: "1036118675043",
-  appId: "1:1036118675043:web:3bdffe3a412f1393763256",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
